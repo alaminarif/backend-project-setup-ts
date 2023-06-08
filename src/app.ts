52224c425c53f80c3ea3fd222b31c7e4af6e3179
al-amin <arifurr231@gmail.com>
2023-06-08 11:45:32 +0600

    error handle && zod done

--- src/app.ts
+++ src/app.ts
@@ -4,35 +4,20 @@ import express, {
   Request,
   Response,
   urlencoded,
-} from 'express'
-import cors from 'cors'
-import globalErrodHandler from './app/middleware/errorHandler'
-const app: Application = express()
+} from 'express';
+import cors from 'cors';
+import globalErrodHandler from './app/middleware/GlobalErrorHandler';
+const app: Application = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
 
 // global error handlle
 
-class ApiError extends Error {
-  statusCode: number
-  constructor(statusCode: number, message: string | undefined) {
-    super(message)
-    this.statusCode = statusCode
-    if (stack) {
-      this.stack = stack
-    } else {
-      Error.captureStackTrace(this, this.constructor)
-    }
-  }
-}
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  Promise.reject(new Error('Unhandle Promise Rejection'));
+});
 
-// app.get('/', (req: Request, res: Response) => {
-//   throw new ApiError('error disi')
-//   // res.send('home route okh')
-//   // res.send('home route okh')
-// })
-
-app.use(globalErrodHandler)
-export default app
+app.use(globalErrodHandler);
+export default app;
